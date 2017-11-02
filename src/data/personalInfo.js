ead492d37a757df4135333a4f4fba6b7a2d98722
Mohmed Bamhraz <mohmedbamhrz@gmail.com>
2017-11-02 10:01:17 -0700

    - inital commit

---
+++ src/data/personalInfo.js
@@ -0,0 +1,10 @@
+const personalInfoData = {
+  name: 'Mohammed Bamhraz',
+  linkedIn: 'https://www.linkedin.com/in/mohammed-bamhraz/',
+  github: 'https://github.com/MohammedBm/',
+  angelList: 'https://angel.co/',
+  email: 'mohmedbamhrz@gmail.com',
+  resume: 'https://www.dropbox.co m/'
+};
+
+export { personalInfoData };
