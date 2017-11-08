fb083dee9baf491516a8eb61d6ffab46b485c49d
Mohmed Bamhraz <mohmedbamhrz@gmail.com>
2017-11-07 21:53:21 -0800

    - update MyModal.js  - update main.scss

--- src/components/MyModal.js
+++ src/components/MyModal.js
@@ -11,19 +11,20 @@ export default class MyModal extends Component {
     });
 
     return (
-      <Modal
+      < Modal
         onRequestClose={onRequestClose}
-        effect={Effect.Fall}>
-        <div className="project-container col-lg-6 col-md-12 col-sm-12">
-          <div className="image-container centered" >
-            <div className="screenshot-container">
-              {project.gfycat ? <div className="gfyitem centered" data-id="SilkyNextIrishwolfhound" ></div> : <img className='center-image' src={project.desktop_image} />}
-            </div>
+        className=''
+        effect={Effect.Fall} >
+        <div className="modal-header">
+          <a href={project.project_url} className='link-to-project' target="_blank">
+            <h2>{project.title}</h2>
+          </a>
+        </div>
+        <div className="modal-body">
+          <div className="screenshot-container img-responsive center-block">
+            {project.gfycat ? <div className="gfyitem  " data-id="SilkyNextIrishwolfhound" ></div> : <img className='center-image' src={project.desktop_image} />}
           </div>
           <div className="project-info">
-            <a href={project.project_url} className='link-to-project' target="_blank">
-              <h2 className='project-link'>{project.title}</h2>
-            </a>
             <a href={project.github_url} className='link-to-project' target="_blank">
               {project.github_url !== '' ? 'Github' : null}
             </a>
@@ -37,8 +38,44 @@ export default class MyModal extends Component {
           </div>
         </div>
 
-        <button onClick={ModalManager.close}>Close Modal</button>
-      </Modal>
-    );
+        <div className="modal-footer">
+          <button type="button" className="btn btn-default" onClick={ModalManager.close}>Close</button>
+          <button type="button" className="btn btn-primary" onClick={ModalManager.close}>OK</button>
+        </div>
+      </Modal >
+    )
   }
 }
+
+
+
+{/* <Modal
+  onRequestClose={onRequestClose}
+  className=''
+  effect={Effect.Fall}>
+  <div className="modal-contaier">
+    <div>
+      <div className="screenshot-container">
+        {project.gfycat ? <div className="gfyitem centered  " data-id="SilkyNextIrishwolfhound" ></div> : <img className='center-image' src={project.desktop_image} />}
+      </div>
+    </div>
+    <div className="project-info">
+      <a href={project.project_url} className='link-to-project' target="_blank">
+        <h2 className="modal-header">{project.title}</h2>
+      </a>
+      <a href={project.github_url} className='link-to-project' target="_blank">
+        {project.github_url !== '' ? 'Github' : null}
+      </a>
+      <div className='secondry-text'>{project.date}</div>
+      <h3>Background</h3>
+      <div className='secondry-text'>{project.background}</div>
+      <h3>Role</h3>
+      <div className='secondry-text'>{project.role}</div>
+      <h3>Tech Stack</h3>
+      <div className="tech-icon-container">{techStackItems}</div>
+    </div>
+  </div>
+
+  <button onClick={ModalManager.close}>Close Modal</button>
+</Modal>
+ */}
